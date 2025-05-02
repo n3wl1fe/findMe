0d3faa52e955cfba61f6f621f7fdf0cf1d08e24a
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2025-05-02 14:31:38 +0000

    Added jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+\.tsx?$": ["ts-jest", {}],
+  },
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+};
